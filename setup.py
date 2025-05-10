113e16f1d56fabcaf643bb5bb7288c67f7d2da92
钧泽 <1738407610@qq.com>
2025-05-11 03:33:50 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="轻量、灵活、易上手的Python剪映草稿生成及导出工具，构建全自动化视频剪辑/混剪流水线",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ccmldl/pyJianYingDraft",
+    url="https://github.com/ccmldl/pyJianYingPro",
     packages=find_packages(),
     package_data={
         'pyJianYingPro': ['draft_content_template.json']
